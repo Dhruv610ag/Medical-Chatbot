852b62d0f07d377ebeb897b6c0b3adf3bad1145c
Dhruv610ag <dhruv610agg@gmail.com>
2025-09-15 18:41:50 +0530

    Files Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MedicoAi",
+    version="0.1.0",
+    author="Dhruv Agarwal",
+    author_email="dhruv610agg@gmil.com",
+    packages=find_packages(),
+    install_requires=[]
+)
